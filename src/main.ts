46ed0e64f8291d4bd74babfda3adb8a68f5476a1
vincenciusadyatma <adyatmav@gmail.com>
2026-08-04 20:43:13 +0700

    fix: create server statis for access image from url

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://127.0.0.1:5173',
-    ],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+  app.useStaticAssets(join(process.cwd(), 'storage'), {
+    prefix: '/storage',
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(3000);
 }
 bootstrap();
