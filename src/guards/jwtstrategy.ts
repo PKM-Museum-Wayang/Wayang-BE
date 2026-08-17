ffca5cf3fa06dee35efaeb2362408342adf69669
vincenciusadyatma <adyatmav@gmail.com>
2026-08-17 21:56:51 +0700

    feat: create auth feature for FE

--- src/guards/jwtstrategy.ts
+++ src/guards/jwtstrategy.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import type { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly config: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.access_token;
+        },
+      ]),
 
       secretOrKey: config.get<string>('SECRET_KEY')!,
     });
